d1e4f934be1134c29109a259d2c702598bbab3fd
John Cole <johnwebbcole@gmail.com>
2020-11-23 12:09:00 -0600

    update montage

--- docs.sh
+++ docs.sh
@@ -2,3 +2,5 @@ rm -rf srcx
 cp -R src srcx
 find ./srcx -name "*.js" -print0 | xargs -0 sed -i ".back" "s/@typedef {typeof import/@xtypedef {typeof import/g"
 npm run docs
+
+montage test/images/*.png docs/images/montage.png
